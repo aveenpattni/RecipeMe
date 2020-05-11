ccd9d0ff8b9eb5259d94e07e6c2038488ae99660
Aveen Pattni <aveenpattni@gmail.com>
2020-05-11 15:49:16 -0400

    add api url

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+import { secretKey } from "../config";
+
+const endpoint = "https://edamam-recipe-search.p.rapidapi.com/search";
+
 
 router.get("/test", (req, res) => {
   res.send("Testing");
